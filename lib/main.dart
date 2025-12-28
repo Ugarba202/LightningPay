1aed27bab9a902c1a0f1a121f6cb255211034f2e
Usman Umar Garba <ugarba202@gmail.com>
2025-12-28 09:25:52 +0100

    setting up the backend of the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lighting_pay/features/splash/ui/splashscreen.dart';
+import 'package:lighting_pay/firebase_options.dart';
 
 import 'core/themes/app_themes.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const LightningPayApp());
 }
 
